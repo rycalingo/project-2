d6d29a5073b701d9a1a7db3fb76e84b1007713ee
Marsha Miquela <marshamiquela@Virgilios-MBP.home>
2018-02-25 22:58:15 -0500

    Added route for Questionnaires in server.js and added all-questions.hbs

--- server.js
+++ server.js
@@ -41,6 +41,113 @@ var authRoute = require('./app/routes/auth.js')(app,passport);
 //load passport strategies
 require('./app/config/passport/passport.js')(passport, models.user);
 
+// Data for Questionnaires
+var questions = [
+  {
+    q: "Do you have trouble forgetting in particular recently learned information?"
+  }, {
+    q: "Do you increasingly need to rely on memory aids (e.g., reminder notes or electronic devices) or family members for things they used to handle on their own?"
+  }, {
+    q: "Do you experience changes in your ability to develop and follow a plan, work with numbers, follow a familiar recipe or keeping track of monthly bills ? "
+  }, {
+    q: "Do you sometimes experience trouble driving to a familiar location?"
+  }, {
+    q: "Sometimes do you forget where you are or how you got there?"
+  }, {
+    q: "Are you experiencing difficulty reading, judging distance and determining color or contrast?"
+  }, {
+    q: "Are you having trouble following or joining a conversation ? "
+  }, {
+    q: "Do you lose things and then are unable to go back over your steps to find them again?"
+  }, {
+    q: "Do you have trouble keeping up with a favorite sports team or remembering how to complete a favorite hobby?"
+  }, {
+    q: "Do you lose track of the date or the season?"
+  }, {
+    q: "Do you eat until you feel uncomfortably full ?"
+  }, {  
+    q: "Do you eat large amounts of food when you don’t feel physically hungry ?"
+  }, {  
+    q: "Do you eat alone because you are embarrassed by how much you are eating?"
+  }, {
+    q: "Are you feeling depressed or very guilty after overeating?"
+  }, {
+    q: "Did you feel upset and promise yourself that you will control your eating?"
+  }, {
+    q: "Are you satisfied by your eating patterns?"
+  }, {
+    q: "Does your weight affect the way you feel about yourself?"
+  }, {
+    q: "Have any members of your family suffered from an eating  disorder?"
+  }, {
+    q: "Do you believe yourself to be fat when others say you are thin?"
+  }, {
+    q: "Would you say that food dominates your life?"
+  }, {
+    q: "Over the last several months, heave you be continually worried or been anxious about a number of events or activities in your daily life ?"
+  }, {
+    q: "Are you experiencing the inability to control excessive worry, occurring more days than not, for a least six months ?"
+  }, {
+    q: "Are you bothered by any of the following ? Restlessness, feeling keyed-up, or on edge. Being easily tired Irritability"
+  }, {
+    q: "Have you experienced changes in sleeping or eating habits?"
+}, {
+    q: "Do you often feel sad or depressed?"
+  }, {
+    q: "In the past year has the use of alcohol and drugs resulted in your failure to fulfill responsibilities with work, school, or family?"
+  }, {
+    q: "Has the use of drugs and alcohol gotten you arrested?"
+  }, {
+    q: "Have you driven under the influence of drugs and alcohol?"
+  }, {
+    q: "Do you often feel worthless or guilty?"
+  }, {
+    q: "Do you have problems concentrating?"
+  }, {
+    q: "Has there ever been a period of time when you felt so good or so hyper that other people thought you were not your normal self or you were so hyper that you got into trouble ?"
+}, {
+    q: "Have you been so irritable that you shouted at people or started fights or arguments ?"
+  }, {
+    q: "Do you feel much more self - confident than usual ?"
+  }, {
+    q: "Have you experienced being more talkative or spoke much faster than usual?"
+  }, {
+    q: "Do you have racing thoughts or been unable to slow your mind down?"
+  }, {
+    q: "Have you been much more active or did many more things than usual?"
+  }, {
+    q: "Have you done things that were unusual for you or that other people might have thought was excessive, foolish, or risky?"
+  }, {
+    q: "Have any of your blood relatives (i.e. children, siblings, parents, grandparents, aunts, uncles) had manic-depressive illness or bipolar disorder?"
+  }, {
+    q: "Have you spent money compulsively in a way that got you or your family into trouble?"
+  }, {
+    q: "Do you find that you are more social or outgoing than usual, for example, you telephoned friends in the middle of the night?"
+  }, {
+    q: "Do you hear, see, or feel things that do not exist ?"
+  }, {
+    q: "Do you have false beliefs, especially based on fear or suspicion of things that are not real ?"
+  }, {
+    q: "Do you jump between unrelated topics, making strange connections between thoughts ?" 
+  }, {
+    q: "Are you unduly suspicious of individuals or organizations, believing them to be plotting to cause them harm?"
+  }, {
+    q: "Do you have clearly false but strongly held belief in having a special power or authority - for instance, they may believe that they are a world leader?"
+  }, {
+    q: "Are you becoming more socially withdrawn?"
+  }, {
+    q: "Are you performing worse for a sustained period at school or work?"
+  }, {
+    q: "Have you becoming more distressed or agitated yet been unable to explain why?"
+  }, {
+    q: "Do you often exhibit odd behavior, such as repeating the same movements, or in other cases, do you stand still in the same position for hours?"
+  }, {
+    q: "Do you believe that different colors convey different messages?"
+  },                            
+];
+
+
+
 //Sync Database
 models.sequelize.sync().then(function () {
 
@@ -52,6 +159,11 @@ models.sequelize.sync().then(function () {
 
 });
 
+app.get("/category1", function (req, res) {
+    res.render("all-questions", {
+        questionnaire: questions,
+    });
+});
 
 app.get('/', function (req, res) {
 
