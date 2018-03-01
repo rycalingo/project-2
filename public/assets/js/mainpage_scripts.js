32c5cba9f470775ddf2379f8eb05036002fd3f1b
Ryan Calingo <gryan.calingo@gmail.com>
2018-03-01 08:41:01 -0500

    main page styling

---
+++ public/assets/js/mainpage_scripts.js
@@ -0,0 +1,10 @@
+
+// $(document).ready(function(){
+        
+//     var homeBanner = $('.home-banner');
+        
+//     setTimeout({
+//         $homeBanner.addClass("FADE-IN");
+//     },500);
+    
+// });
